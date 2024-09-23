394afe5d84588ce868286d2f3eccda6edb4ad7e8
mezz01 <mezzatamine2002@gmail.com>
2024-09-23 23:38:30 +0100

    added content to about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,34 @@
 export default function About() {
     return(
-        <section>
+        <section className="" >
+            <h1>
+                About me.
+            </h1>
+            <h3>
+                I'm a developer, UI/UX designer and a Photo/Videography enthusiast.
+            </h3>
+            <div>
+                <p>Year 0</p>
+                <p> Grew up as a computer kid, had one since birth because my mom wanted to learn Microsoft word and Excel so my dad bought her one, spent a lot of time playing and trying to understand more about what is this windows thing.</p>
+            </div>
+            <div>
+                <p>2019</p>
+                <p> Fast forward 18 years, I finished highschool in physics and then pursued this programming thing in college.</p> 
+            </div>
+            <div>
+                <p>2021</p>
+                <p>Two years later got my first diploma in programming and Learned a lot about different programming languages from mobile to website, front-end and backend.</p> 
+            </div>
+            <div>
+                <p>2022</p>
+                <p>Started my journey in 1337'42school' after succeeding in the piscine of 2021 which was a set of C and linux commands exams and exercises for a whole month.
+                    I learned all valuable things in my journey at 1337KH, from low level programming in C and using different algorithms and Data structures, to Git and linux command lines.
+                    But more importantly team work and peerLearning, using only projects and google as our friends 'which were like family there'.
+                </p> 
+            </div>
+
             
+           
         </section>
         
     )
