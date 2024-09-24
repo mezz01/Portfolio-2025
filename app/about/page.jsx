45afe310f0bffb1afd1624a179250cb2e080a23b
mezz01 <mezzatamine2002@gmail.com>
2024-09-24 04:04:10 +0100

    push back

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,35 +1,52 @@
+import Image from "next/image";
+
 export default function About() {
     return(
-        <section className="" >
-            <h1>
-                About me.
-            </h1>
-            <h3>
-                I'm a developer, UI/UX designer and a Photo/Videography enthusiast.
-            </h3>
-            <div>
+    <main>
+        <section className="h-full" >
+            <div className="h-full flex m-5 mt-24 flex-start flex-col">
+                <h1 className="text-2xl underline decoration-4 lg:decoration-8 decoration-BrandPurple font-black md:text-4xl lg:text-6xl">
+                    About me<span className="text-BrandPurple no-underline">.</span>
+                </h1>
+                <h3 className="text-lg font-serif md:text-xl lg:text-3xl mt-10">
+                    I'm a developer, UI/UX designer and a Photo/Videography enthusiast.
+                </h3>
+            </div>
+            <div className="flex justify-start mt-96">
                 <p>Year 0</p>
                 <p> Grew up as a computer kid, had one since birth because my mom wanted to learn Microsoft word and Excel so my dad bought her one, spent a lot of time playing and trying to understand more about what is this windows thing.</p>
+                <div className="relative">
+                    <Image 
+                        
+                    />
+                </div>
             </div>
-            <div>
+            <div className="flex justify-start mt-96">
                 <p>2019</p>
                 <p> Fast forward 18 years, I finished highschool in physics and then pursued this programming thing in college.</p> 
             </div>
-            <div>
+            <div className="flex justify-start mt-96">
                 <p>2021</p>
                 <p>Two years later got my first diploma in programming and Learned a lot about different programming languages from mobile to website, front-end and backend.</p> 
             </div>
-            <div>
+            <div className="flex justify-start mt-96">
                 <p>2022</p>
                 <p>Started my journey in 1337'42school' after succeeding in the piscine of 2021 which was a set of C and linux commands exams and exercises for a whole month.
                     I learned all valuable things in my journey at 1337KH, from low level programming in C and using different algorithms and Data structures, to Git and linux command lines.
                     But more importantly team work and peerLearning, using only projects and google as our friends 'which were like family there'.
-                </p> 
+                </p>
             </div>
-
-            
-           
+            <div className="flex justify-start mt-96">
+                <p>2024</p>
+                <p>
+                    A creative fullstack web developer was born and hopes for more achievements in his life.
+                </p>
+            </div>
         </section>
+        <section className="photography h-full ">
+
+        </section>
+    </main>
         
     )
 }
--- components/menu-button.jsx
+++ components/menu-button.jsx
@@ -19,14 +19,13 @@ export default function Button(){
             <motion.div
                 className={`${isActive ? 'hidden' : ''}`}
                 animate={{ opacity: isActive ? 0 : 1, x: isActive ? -100 : 0, scale: isActive ? 2 : 1 }}
-
                 transition={{ duration: 0.4, ease: "easeOut" }}
             >
-                <ul     className={`h-96 w-64 top-0 font-sans font-light text-sm lg:text-lg cursor-pointer transition-transform duration-500`}>
-                    <li className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out"><Link href="/about">About</Link></li>
-                    <li className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" ><Link href="/">Projects</Link></li>
-                    <li className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" ><Link href="/contact">Contact</Link></li>
-                    <li className="text-2xl mx-4 pt-9  rounded-2xl lg:mx-9 hover:scale-110 hover:underline duration-150  ease-in-out" ><Link href="/"></Link>See my resume</li>
+                <ul className={`h-96 w-64 top-0 font-sans font-light text-sm lg:text-lg cursor-pointer transition-transform duration-500`}>
+                    <li><Link href="/about"><button  className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" onClick={() => {Toggle()}}>About</button></Link></li>
+                    <li><Link href="/contact"><button  className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" onClick={() => {Toggle()}}>Contact</button></Link></li>
+                    <li><Link href="/projects"><button  className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" onClick={() => {Toggle()}}>Projects</button></Link></li>
+                    <li><Link href="/about"><button  className="text-2xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:underline duration-150  ease-in-out" onClick={() => {Toggle()}}>See my resume</button></Link></li>
                 </ul>
             </motion.div>
         </div>
