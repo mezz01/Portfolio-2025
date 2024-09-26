2c7f7760ce3a4fa021832415ab1bd1938e0dc631
mezz01 <mezzatamine2002@gmail.com>
2024-09-26 16:23:07 +0100

    fixed

--- components/menu-button.jsx
+++ components/menu-button.jsx
@@ -23,7 +23,7 @@ export default function Button(){
                     <li><Link href="/about"><button  className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" onClick={() => {Toggle()}}>About</button></Link></li>
                     <li><Link href="/contact"><button  className="text-6xl mx-4 pt-4 lg:mx-9 text-gray-400" onClick={() => {Toggle()}} >Blog</button></Link></li>
                     <li><Link href="/projects"><button  className="text-6xl mx-4 pt-4 lg:mx-9 hover:scale-110 hover:line-through duration-150  ease-in-out" onClick={() => {Toggle()}}>Projects</button></Link></li>
-                    <li><a href="/CV.pdf" download=""><button  className="text-2xl mx-4 pt-4 mt-10 lg:mx-9 hover:scale-110 hover:underline duration-150  ease-in-out" onClick={() => {Toggle()}}>See my resume</button></a></li>
+                    <li><a href="/CV.pdf" download="CV-Mohamed-Amine.pdf"><button  className="text-2xl mx-4 pt-4 mt-10 lg:mx-9 hover:scale-110 hover:underline duration-150  ease-in-out" onClick={() => {Toggle()}}>See my resume</button></a></li>
                 </ul>
             </motion.div>
         </div>
