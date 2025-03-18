d2bf60c452b206235bd9ba91e8c1aef653d8018e
Amine Mezzat <86370095+mezz01@users.noreply.github.com>
2025-03-18 00:18:35 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en" className="mt-0">
+    <meta name="google-site-verification" content="KF0iAdcfp0tLpz6b13YTLJBdNnuXlQm7M-C_mQMpi6c" />
       <body>
         <Header />
         {children}
